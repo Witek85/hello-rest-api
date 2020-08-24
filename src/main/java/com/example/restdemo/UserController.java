f4689dd453b7072802d90751719f0874f9256cf5
Witek85 <witold.szklarski@gmail.com>
2020-08-25 00:31:59 +0200

    init

---
+++ src/main/java/com/example/restdemo/UserController.java
@@ -0,0 +1,34 @@
+package com.example.restdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/api/users")
+    public Iterable<User> getUsers() {
+
+        User user1 = new User();
+        user1.setEmail("test@mail.pl");
+        user1.setName("Janusz");
+        userRepository.save(user1);
+
+        User user2 = new User();
+        user2.setEmail("testmail@mail.pl");
+        user2.setName("Marian");
+        userRepository.save(user2);
+
+        return userRepository.findAll();
+    }
+
+}
